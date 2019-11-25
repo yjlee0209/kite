9f6dc8ea49f72643066ac56e83da782039a81f83
YJLee <tetrodotoxin0209@gmail.com>
2019-11-25 15:35:27 +0900

    Create PhoneCompanyInfor.java

---
+++ java_project/PhoneBook/src/ver04/PhoneCompanyInfor.java
@@ -0,0 +1,25 @@
+package ver04;
+
+public class PhoneCompanyInfor extends PhoneInfor{
+	
+	// 회사      company      String
+	String company;
+
+	public PhoneCompanyInfor(String name, String phoneNumber, String email, String company) {
+		super(name, phoneNumber, email);
+		this.company = company;
+	}
+
+	@Override
+	void showInfor() {
+		super.showBasicInfor();
+		System.out.println("회사 : " + this.company);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
