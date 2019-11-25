ff9c27d20429f722edfc08de60e75a6604360e44
YJLee <tetrodotoxin0209@gmail.com>
2019-11-25 15:35:33 +0900

    Create Menu.java

---
+++ java_project/PhoneBook/src/ver05/Menu.java
@@ -0,0 +1,15 @@
+package ver05;
+
+public interface Menu {
+
+	// 인터페이스의 변수 맴버는 상수이어야 한다.
+	public static final int INSERT_UNIV=1;
+	int INSERT_COM=2;
+	int SEARCH=3;
+	int DELETE=4;
+	int LIST=5;
+	int EXIT=6;
+
+	
+	
+}
