b8918f2b8a49b0a34e354128c953f28a5112d6dc
YJLee <tetrodotoxin0209@gmail.com>
2019-11-25 15:35:32 +0900

    Create PhoneUnivInfor.java

---
+++ java_project/PhoneBook/src/ver04/PhoneUnivInfor.java
@@ -0,0 +1,38 @@
+package ver04;
+
+public class PhoneUnivInfor extends PhoneInfor {
+
+	//	주소             address      String
+	//	전공             major        String
+	//	학년             year         String
+	String address;
+	String major;
+	String year;
+	
+//	public PhoneUnivInfor(String name, String phoneNumber, String email) {
+//		super(name, phoneNumber, email);	
+//	}
+
+	public PhoneUnivInfor(String name, String phoneNumber, String email,
+			String address, String major, String year) {
+		super(name, phoneNumber, email);
+		this.address = address;
+		this.major = major;
+		this.year = year;
+	}
+
+	@Override
+	void showInfor() {
+		super.showBasicInfor();
+		System.out.println("주소 : " + this.address);
+		System.out.println("전공 : " + this.major);
+		System.out.println("학년 : " + this.year);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
